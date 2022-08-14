e597e0001bba480f56bdd8292984b1bd0ee4072a
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-08-14 16:22:42 +0530

    AwesomeToast Framework added version 2.1.2

---
+++ Sources/AwesomeToast.xcframework/ios-arm64_x86_64-simulator/AwesomeToast.framework/Headers/AwesomeToast.h
@@ -0,0 +1,18 @@
+//
+//  AwesomeToast.h
+//  AwesomeToast
+//
+//  Created by MANAS VIJAYWARGIYA on 14/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AwesomeToast.
+FOUNDATION_EXPORT double AwesomeToastVersionNumber;
+
+//! Project version string for AwesomeToast.
+FOUNDATION_EXPORT const unsigned char AwesomeToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeToast/PublicHeader.h>
+
+
